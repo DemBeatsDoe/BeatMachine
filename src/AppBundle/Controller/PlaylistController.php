05d7531d402c7bdd54c22a8bc7f83bf1ad93b3dc
alistair <aht32@bath.ac.uk>
2017-03-05 17:49:07 +0000

    Playlist controller responds to the id value passed within the GET request. Playlist view then reflects the value the server received.

--- src/AppBundle/Controller/PlaylistController.php
+++ src/AppBundle/Controller/PlaylistController.php
@@ -4,17 +4,22 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlaylistController extends Controller
 {
     /**
      * @Route("/playlist")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        //Store the playlist id
+        $playlistID = $request->query->get('id');
+
         $playlistSongs = array("Song1", "Song2", "Song3");
 
         return $this->render('users_playlist.html.twig', array(
+            'playlistID' => $playlistID,
             'playlistName' => 'Test Playlist',
             'playlistAuthor' => 'User1',
             'playlistSongs' => $playlistSongs,
