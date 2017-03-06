717c876a2889c72734e940b9a83453800df01d97
Matt Buckley <mbuckley2000@gmail.com>
2017-03-06 15:42:57 +0000

    Added playbar - no controls yet

--- src/AppBundle/Controller/DBTestController.php
+++ src/AppBundle/Controller/DBTestController.php
@@ -12,6 +12,7 @@ use AppBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bridge\Doctrine;
 
 class DBTestController
 {
